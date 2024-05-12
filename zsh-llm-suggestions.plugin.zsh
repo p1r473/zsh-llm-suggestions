be1ce72424b30cf7100656e09b248f138d598d7f
p1r473 <subwayjared@gmail.com>
2024-05-12 01:10:10 -0400

    Update zsh-llm-suggestions.plugin.zsh

--- zsh-llm-suggestions.plugin.zsh
+++ zsh-llm-suggestions.plugin.zsh
@@ -1,1 +1,4 @@
 source ${0:A:h}/zsh-llm-suggestions.zsh
+bindkey '^o' zsh_llm_suggestions_ollama
+bindkey '^[^o' zsh_llm_suggestions_ollama_explain
+bindkey '^p' zsh_llm_suggestions_ollama_freestyle
